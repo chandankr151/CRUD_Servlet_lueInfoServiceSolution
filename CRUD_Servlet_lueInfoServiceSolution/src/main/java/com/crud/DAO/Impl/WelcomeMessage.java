db0910d767bbe6e7f4ce027d38146dbc21268352
mithu <mithu@DESKTOP-K7TIBJI>
2024-03-24 15:39:18 +0530

    Added new package com.crud.DAO.Impl In which created one java class having message welcome.

---
+++ CRUD_Servlet_lueInfoServiceSolution/src/main/java/com/crud/DAO/Impl/WelcomeMessage.java
@@ -0,0 +1,9 @@
+package com.crud.DAO.Impl;
+
+public class WelcomeMessage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi, Chandan Welcome to JAVA Full Stack Development");
+	}
+}
