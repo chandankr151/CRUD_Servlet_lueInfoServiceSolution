818b037406e27f8a8041c2e053a8fcc6d1fd96c0
Chandan DAS Tanti <65387190+chandankr151@users.noreply.github.com>
2024-03-24 15:42:41 +0530

    Update WelcomeMessage.java

--- CRUD_Servlet_lueInfoServiceSolution/src/main/java/com/crud/DAO/Impl/WelcomeMessage.java
+++ CRUD_Servlet_lueInfoServiceSolution/src/main/java/com/crud/DAO/Impl/WelcomeMessage.java
@@ -5,5 +5,6 @@ public class WelcomeMessage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi, Chandan Welcome to JAVA Full Stack Development");
+		System.out.println("Thanks");
 	}
 }
